d91398ea30f8267b3e01e332163f5c7668c91563
Adam Pollak <pollakaj@dukes.jmu.edu>
2024-04-10 10:57:34 -0400

    final

---
+++ CS349 Final Project/src/resources/Marker.java
@@ -0,0 +1,13 @@
+package resources;
+
+/**
+ * Marker resource class.
+ *
+ * @author Adam Pollak
+ * @version Final Project
+ *
+ * This work complies with the JMU Honor Code.
+ */
+public class Marker
+{
+}
