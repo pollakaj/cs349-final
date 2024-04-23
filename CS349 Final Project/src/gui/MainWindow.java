e5b9e4788b416fcbb4bb7163097dcb7061267811
Adam Pollak <pollakaj@dukes.jmu.edu>
2024-04-23 00:15:48 -0400

    clean up

--- CS349 Final Project/src/gui/MainWindow.java
+++ CS349 Final Project/src/gui/MainWindow.java
@@ -3,8 +3,6 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
