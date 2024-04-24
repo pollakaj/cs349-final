e9f6f8494a365648db26eea892387007ea696d35
ColeGlaccum <glaccucj@dukes.jmu.edu>
2024-04-24 12:57:29 -0400

    Refactoring BizzareAdventures a bit

--- CS349 Final Project/src/bizarreAdventures/BizarreAdventuresApplication.java
+++ CS349 Final Project/src/bizarreAdventures/BizarreAdventuresApplication.java
@@ -1,12 +1,13 @@
 package bizarreAdventures;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import app.JApplication;
 import io.ResourceFinder;
 import resources.Marker;
 
-public abstract class BizarreAdventuresApplication extends JApplication implements ActionListener
+public class BizarreAdventuresApplication extends JApplication implements ActionListener
 {
   public static final int WIDTH = 600;
   public static final int HEIGHT = 800;
@@ -15,5 +16,24 @@ public abstract class BizarreAdventuresApplication extends JApplication implemen
   {
     super(args, WIDTH, HEIGHT);
     ResourceFinder rf = ResourceFinder.createInstance(new Marker());
+  }
+  
+  public static void main(String[] args) {
+    JApplication mainWindow = new BizarreAdventuresApplication(args);
+    invokeInEventDispatchThread(mainWindow);
+ }
+
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void init()
+  {
+    // TODO Auto-generated method stub
+    
   }
 }
