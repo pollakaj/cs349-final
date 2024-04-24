c8f3cf073c492b339246f2785b6dba1335e0a21b
ColeGlaccum <glaccucj@dukes.jmu.edu>
2024-04-24 15:57:49 -0400

    Bizzare Refactor

--- CS349 Final Project/src/bizarreAdventures/BizarreAdventuresApplication.java
+++ CS349 Final Project/src/bizarreAdventures/BizarreAdventuresApplication.java
@@ -1,21 +1,27 @@
 package bizarreAdventures;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
 
 import app.JApplication;
 import io.ResourceFinder;
 import resources.Marker;
+import visual.VisualizationView;
+import visual.dynamic.described.Stage;
+import visual.statik.sampled.Content;
+import visual.statik.sampled.ContentFactory;
 
 public class BizarreAdventuresApplication extends JApplication implements ActionListener
 {
-  public static final int WIDTH = 600;
-  public static final int HEIGHT = 800;
+  public static final int WIDTH = 1920;
+  public static final int HEIGHT = 1080;
 
   public BizarreAdventuresApplication(final String[] args)
   {
     super(args, WIDTH, HEIGHT);
-    ResourceFinder rf = ResourceFinder.createInstance(new Marker());
   }
   
   public static void main(String[] args) {
@@ -33,7 +39,17 @@ public class BizarreAdventuresApplication extends JApplication implements Action
   @Override
   public void init()
   {
-    // TODO Auto-generated method stub
-    
+    ResourceFinder rf = ResourceFinder.createInstance(new Marker());
+    ContentFactory factory = new ContentFactory(rf);
+    Stage stage = new Stage(50);
+    Content background = factory.createContent("349 Background.png", 3, false);
+    stage.add(background);
+    VisualizationView stageView = stage.getView(); 
+    stageView.setBounds(0, 0, WIDTH, HEIGHT);
+    Content platform = factory.createContent("large_platform.png", 4, false);
+    stage.add(platform);
+    JPanel contentPane = (JPanel)this.getContentPane();
+    contentPane.add(stageView);
+    stage.start();
   }
 }
--- CS349 Final Project/src/resources/Marker.java
+++ CS349 Final Project/src/resources/Marker.java
@@ -10,4 +10,5 @@ package resources;
  */
 public class Marker
 {
+  
 }
