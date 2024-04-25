8de84bba43efce88827f42b9872eba4fbd0e8acf
Cole Glaccum <glaccucj@dukes.jmu.edu>
2024-04-24 22:21:09 -0500

    Basic Clip stuff, not working

--- CS349 Final Project/src/bizarreAdventures/BizarreAdventuresApplication.java
+++ CS349 Final Project/src/bizarreAdventures/BizarreAdventuresApplication.java
@@ -3,10 +3,17 @@ package bizarreAdventures;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JPanel;
 
 import app.JApplication;
+import auditory.sampled.BufferedSound;
+import auditory.sampled.BufferedSoundFactory;
 import characters.Bernie;
 import io.ResourceFinder;
 import resources.Marker;
@@ -41,8 +48,30 @@ public class BizarreAdventuresApplication extends JApplication implements Action
   public void init()
   {
     ResourceFinder rf = ResourceFinder.createInstance(new Marker());
+    BufferedSoundFactory buffFactory = new BufferedSoundFactory(rf);
+
+    BufferedSound backgroundMusic;
+    Clip clip = null;
+    
+    try {
+		backgroundMusic = buffFactory.createBufferedSound("background_music.mp3");
+		backgroundMusic.render(clip);
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (UnsupportedAudioFileException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (LineUnavailableException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
+
     ContentFactory factory = new ContentFactory(rf);
     Stage stage = new Stage(50);
+    
     
     Bernie b = new Bernie();
     Content background = factory.createContent("349 Background.png", 3, false);
@@ -52,10 +81,12 @@ public class BizarreAdventuresApplication extends JApplication implements Action
     stageView.setBounds(0, 0, WIDTH, HEIGHT);
     
     Content platform = factory.createContent("large_platform.png", 4, false);
+    platform.setLocation(200, 600);
     stage.add(platform);
     
     JPanel contentPane = (JPanel)this.getContentPane();
     contentPane.add(stageView);
     stage.start();
   }
+
 }
