a5d58d02e7e61af77505baab1b4e151e3e3177e4
Adam Pollak <pollakaj@dukes.jmu.edu>
2024-04-26 15:53:46 -0400

    slicing images + better movement

--- CS349 Final Project/src/characters/Bernie.java
+++ CS349 Final Project/src/characters/Bernie.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,27 +15,41 @@ import javax.imageio.ImageIO;
 
 import io.ResourceFinder;
 import visual.dynamic.described.RuleBasedSprite;
+import visual.dynamic.described.SampledSprite;
 import visual.statik.sampled.*;
 
 public class Bernie extends RuleBasedSprite implements KeyListener, ActionListener
 {
-  private static final int SPEED = 8;
-  private static final double GRAVITY = 1.5;
-  private static final double INIT_JUMP_SPD = -25.0;
+  private static final int SPEED = 9;
+  private static final double GRAVITY = 4.0;
+  private static final double INIT_JUMP_SPD = -40.0;
   
   private double jumpSpeed = INIT_JUMP_SPD;
   private boolean isJumping = false;
+  private boolean movingLeft = false;
+  private boolean movingRight = false;
   private int startY = 650;
-  private int startX = 100;
+  private SampledSprite slicingBern;
+  private Content content2;
+  private Content content3;
+  private Point2D local;
 
   public Bernie()
   {
     super(new Content());
     BufferedImage bernie = null;
+    BufferedImage slice1 = null;
+    BufferedImage slice2 = null;
     try
     {
       bernie = ImageIO.read(new File("src/resources/Bern.png"));
       bernie = resizeImage(bernie, 125, 150);
+      
+      slice1 = ImageIO.read(new File("src/resources/Bern(slice1).png"));
+      slice1 = resizeImage(bernie, 125, 150);
+      
+      slice2 = ImageIO.read(new File("src/resources/Bern(slice2).png"));
+      slice2 = resizeImage(bernie, 125, 150);
     }
     catch (IOException e)
     {
@@ -47,7 +62,16 @@ public class Bernie extends RuleBasedSprite implements KeyListener, ActionListen
     finder = ResourceFinder.createInstance(new resources.Marker());
     factory = new ContentFactory(finder);
     content = factory.createContent(bernie);
+    content2 = factory.createContent(slice1);
+    content3 = factory.createContent(slice2);
+    
+    local = new Point2D.Double(this.x, this.y);
+    slicingBern = new SampledSprite();
+    slicingBern.addKeyTime(0, local, null, null, content);
+    
     this.content = content;
+    slicingBern.setVisible(true);
+    this.addAntagonist(slicingBern);
     this.setLocation(100, 650);
     this.setVisible(true);
   }
@@ -84,28 +108,37 @@ public class Bernie extends RuleBasedSprite implements KeyListener, ActionListen
     int code = e.getKeyCode();
     if (code == KeyEvent.VK_LEFT)
     {
-      this.x -= SPEED;
+      movingLeft = true;
     }
     if (code == KeyEvent.VK_RIGHT)
     {
-      this.x += SPEED;
+      movingRight = true;
     }  
     if (code == KeyEvent.VK_SPACE && !isJumping && this.y == startY)
     {
       isJumping = true;
+    }
+    if (code == KeyEvent.VK_X)
+    {
+      slicingBern.addKeyTime(200, local, null, null, content2);
+      slicingBern.addKeyTime(400, local, null, null, content3);
     }
   }
 
   @Override
   public void keyReleased(KeyEvent e)
   {
-    // TODO Auto-generated method stub
-    
+    int code = e.getKeyCode();
+    if (code == KeyEvent.VK_LEFT) movingLeft = false;
+    if (code == KeyEvent.VK_RIGHT) movingRight = false;
   }
 
   @Override
   public void handleTick(int arg0)
   {
+    if (movingLeft) this.x -= SPEED;
+    if (movingRight) this.x += SPEED;
+
     if (isJumping)
     {
       jumpSpeed += GRAVITY;
