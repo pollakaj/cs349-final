7e50d6e9f8d96ae2dbf42bbac7558a498cccad0a
Adam Pollak <pollakaj@dukes.jmu.edu>
2024-04-29 07:43:38 -0400

    jump

--- CS349 Final Project/src/characters/Bernie.java
+++ CS349 Final Project/src/characters/Bernie.java
@@ -1,12 +1,10 @@
 package characters;
 
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -17,24 +15,19 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.Timer;
 
 import Components.GameOver;
-import Components.Platform;
 import auditory.sampled.BufferedSound;
 import auditory.sampled.BufferedSoundFactory;
-import bizarreAdventures.BizarreAdventuresApplication;
 import io.ResourceFinder;
 import visual.dynamic.described.RuleBasedSprite;
-import visual.dynamic.described.SampledSprite;
 import visual.dynamic.described.Sprite;
 import visual.dynamic.described.Stage;
-import visual.dynamic.described.TweeningSprite;
-import visual.dynamic.sampled.RectangleWipe;
 import visual.statik.sampled.*;
 
 public class Bernie extends RuleBasedSprite implements KeyListener, ActionListener
 {
   private static final int SPEED = 9;
   private static final double GRAVITY = 4.0;
-  private static final double INIT_JUMP_SPD = -45.0;
+  private static final double INIT_JUMP_SPD = -50.0;
   
   private Content content1;
   private Content leftContent;
